b6e3358f168baf5c5fc8f5fa3233a79307abd656
aabbabar <azlee.babar@bbc.co.uk>
2024-04-18 11:20:32 +0100

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import './App.css';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const ListItem = styled.li`
+  list-style-type: none;
+  border: 1px solid #aaa;
+  padding: 5px;
+  margin-bottom: 10px;
+`;
+const List = styled.ol`
+  border: 2px solid grey;
+  padding: 15px;
+  min-height: 450px;
+  min-width: 200px;
+`;
+
+const Button = styled.button`
+  padding: 0px;
+  margin-left: 10px;
+  min-width: 25px;
+  min-height: 25px;
+`
+const AcceptButton = styled(Button)`
+  background-color: darkgreen;
+`
+const DeclineButton = styled(Button)`
+  background-color: darkred;
+`
+
+const Suggestion = ({ suggestion, acceptSuggestion, declineSuggestion }) => (
+  <ListItem>
+    {suggestion.title}
+    <AcceptButton onClick={() => acceptSuggestion(suggestion)}
+                  aria-label={`Accept ${suggestion.title}`}>✔</AcceptButton>
+    <DeclineButton onClick={() => declineSuggestion(suggestion)}
+                   aria-label={`Decline ${suggestion.title}`}>𝗫</DeclineButton>
+  </ListItem>
+);
+const SuggestionsList = ({ suggestions, acceptSuggestion, declineSuggestion, setAnnouncement }) => (
+  <List aria-label={`${suggestions.length} suggestions available`}>
+    {suggestions.map(s =>
+      <Suggestion
+        suggestion={s}
+        acceptSuggestion={acceptSuggestion}
+        declineSuggestion={declineSuggestion}
+        setAnnouncement={setAnnouncement}
+        key={s.title}
+      />)}
+  </List>
+);
+
+const Tag = ({ tag, removeTag }) => (
+  <ListItem>
+    {tag.title}
+    <DeclineButton onClick={() => removeTag(tag)}>𝗫</DeclineButton>
+  </ListItem>
+);
+const TagsList = ({ tags, removeTag }) => (
+  <List>
+    {tags.map(t => <Tag tag={t} removeTag={removeTag} key={t.title}/>)}
+  </List>
+);
+
+const Announcer = styled.p`
+  position: absolute;
+  top: -9999px;
+  left: -9999px;
+`
+const StyledApp = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  min-width: 550px;
+`
+
+const generateSuggestions = (n) => [...Array(n)].map((s, i) => ({ title: `suggestion ${i + 1}` }));
+const generateTags = (n) => [...Array(n)].map((s, i) => ({ title: `tag ${i + 1}` }));
+const suggestionToTag = (s) => ({ title: `tag ${s.title.split(' ')[1]}` })
+
+function App() {
+  const [announcement, setAnnouncement] = useState('')
+  const [suggestions, setSuggestions] = useState(generateSuggestions(9));
+  const [tags, setTags] = useState(generateTags(0));
+  const removeTag = (t) => setTags(tags.filter(s => s.title !== t.title));
+  const removeSuggestion = (s) => setSuggestions(suggestions.filter(t => s.title !== t.title));
+  const acceptSuggestion = (s) => {
+    setTags([...tags, suggestionToTag(s)])
+    removeSuggestion(s)
+    setAnnouncement(`${s.title} accepted`)
+  }
+
+  return (
+    <StyledApp>
+      <Announcer aria-live="polite" aria-label={announcement}></Announcer>
+      <TagsList tags={tags} removeTag={removeTag}/>
+      <SuggestionsList
+        suggestions={suggestions}
+        acceptSuggestion={acceptSuggestion}
+        declineSuggestion={removeSuggestion}
+      />
+    </StyledApp>);
+}
+
+export default App;
